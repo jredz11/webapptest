dd9af260cc2ca6edf06be88b00f30d6a32eed08e
itexpertshire <itexperts.hire@gmail.com>
2019-02-17 14:28:52 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.5/dist-packages
-python /root/.m2/EDAS.py $JENKINS_HOME/workspace/webapptest/target/webapptest.war
+python3 /root/.m2/EDAS.py $JENKINS_HOME/workspace/webapptest/target/webapptest.war
